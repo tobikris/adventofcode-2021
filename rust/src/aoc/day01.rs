43a414f82dac1b5b15b99daf95ff6683374d14d0
Tobias Krischer <tobias.krischer@elyxon.de>
2021-12-01 11:44:57 +0100

    add tests and refactor

--- rust/src/aoc/day01.rs
+++ rust/src/aoc/day01.rs
@@ -8,11 +8,7 @@ pub fn main(day: usize) {
 pub fn challenge1(day: usize) -> String {
     let integers = input::as_ints(day, 1);
     if let Ok(integers) = integers {
-        let increases = integers
-            .windows(2)
-            .map(|w| w[0] < w[1])
-            .filter(|w| *w)
-            .count();
+        let increases = count_depth_increases_win(&integers, 2);
         return format!("Increasing depth: {} times", increases);
     }
     "".to_string()
@@ -21,15 +17,37 @@ pub fn challenge1(day: usize) -> String {
 pub fn challenge2(day: usize) -> String {
     let integers = input::as_ints(day, 1);
     if let Ok(integers) = integers {
-        let increases = integers
-            .windows(3)
-            .map(|w| w[0] + w[1] + w[2])
-            .collect::<Vec<_>>()
-            .windows(2)
-            .map(|w| w[0] < w[1])
-            .filter(|w| *w)
-            .count();
+        let increases = count_depth_increases_win(&integers, 4);
         return format!("Increasing depth in windows of 3: {} times", increases);
     }
     "".to_string()
+}
+
+fn count_depth_increases_win(measurements: &Vec<i64>, window: usize) -> usize {
+    measurements
+        .windows(window)
+        .filter_map(|w| match w.first().copied() < w.last().copied() {
+            true => Some(true),
+            false => None,
+        })
+        .count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn challenge1() {
+        let numbers = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        let count = count_depth_increases_win(&numbers, 2);
+        assert_eq!(count, 7)
+    }
+
+    #[test]
+    fn challenge2() {
+        let numbers = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        let count = count_depth_increases_win(&numbers, 4);
+        assert_eq!(count, 5);
+    }
 }
